30980b743f48bd92b1a1ea57ce29ba0569bfd2c5
SultanbaevS <saidtasul99@gmail.com>
2023-08-30 19:30:33 +0500

    example without inherited widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:inherit_lesson/src/widgets/components/app_state_widget.dart';
-
-import 'widgets/store_page.dart';
+import 'widgets/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppStateWidget(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Store',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const StorePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Store',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: Example(),
     );
   }
 }
